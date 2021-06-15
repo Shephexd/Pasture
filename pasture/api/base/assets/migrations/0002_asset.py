89ca522346506134bb7a57ceebc3cba56b1e0715
shephexd <shephexd@gmail.com>
2021-06-15 23:43:53 +0900

    HOTFIX migration file typo error

--- pasture/api/base/assets/migrations/0002_asset.py
+++ pasture/api/base/assets/migrations/0002_asset.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('asset', '0001_initial'),
+        ('assets', '0001_initial'),
     ]
 
     operations = [
