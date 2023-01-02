a488f232a9556d9a7430425182e2737993289adb
shephexd <shephexd@gmail.com>
2023-01-02 16:39:26 +0900

    ADD CORS whitelist

--- pasture/configs/settings/base.py
+++ pasture/configs/settings/base.py
@@ -38,6 +38,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# SECURITY WARNING: Check CORS
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000'
+                         ,'http://localhost:3000']
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
